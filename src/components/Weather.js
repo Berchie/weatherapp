66b0f1d9d0d07aa5ee0988346e509b9eed6dbb87
Berchie <agyemangnti@gmail.com>
2020-10-12 18:13:51 +0000

    add event listenrs

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
+// import Container from 'react-bootstrap/Container'
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
 import InputGroup from 'react-bootstrap/InputGroup'
@@ -11,54 +11,21 @@ import WeatherHistory from '../components/WeatherHistory';
 import '../components/custom.css'
 
 
-
-
 function Weather({setLoggined}) {
 
-    const [searchValue, setSearchValue] = useState('');
-    const [currentWeather, setCurrentWeather] = useState(null);
-    const [currentLocation,  setCurrentLocation] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [city , setCity] = useState(0);
-   
-    
-    const handleCity = (event) => {
-        setSearchValue(event.target.value);
-        
+    const [cityName, setCityName] = useState('Kumasi');
+// const [btValue, setBtnValue] = useState(0);
+
+    const getCityName = (e) => {
+        setCityName(e.target.value);  
+        console.log(cityName);  
     }
 
-    const searchWeather = (e) =>{
+    const submitCallback = (e) =>{
         e.preventDefault();
-        // alert(searchValue);
-        setCity(city + 1);
-        // alert(city)
+        console.log(`City name: ${cityName}`);
     }
-
     
-        
-    
-    useEffect(() => {
-        fetch(`http://api.weatherstack.com/current?access_key=61d337c191d9342d0fbb7d61537f6b00&query=${searchValue}`)
-        .then(respone => respone.json())
-        .then(data => {
-            setCurrentWeather(data.current);
-            setCurrentLocation(data.location);
-            console.log(currentWeather);
-            console.log(currentLocation);            
-            setLoading(false);
-            // if(city === 'value'){
-            //     setCity('');
-            // }
-            
-        })
-        .catch(err => {
-            console.error(err);
-        })
-        
-    },[city]);
-
-
-
     return(
 
         <div className="container">
@@ -70,20 +37,21 @@ function Weather({setLoggined}) {
                             placeholder="City"
                             aria-label="City"
                             aria-describedby="basic-addon2"
-                            value={searchValue}
-                            onChange = {handleCity}
+                            autocomplete="off" 
+                            value={cityName}
+                            onChange={getCityName}
                         />
                         <InputGroup.Append>
-                            <Button variant="secondary"  onClick={searchWeather}>Search</Button>
+                            <Button variant="secondary" onClick={submitCallback}>Search</Button>
                         </InputGroup.Append>
                     </InputGroup>
                 </div>
             </div>
             {/* weather card of a city */}
-            {loading
+            {/* {loading
             ?
             <p>loading...</p>
-            :
+            : */}
             <div className="row justify-content-md-center">
                 <div className="col-md-auto">
                     <Card id="weather-card"  text="white" style={{ width: '13rem', height:'13rem'}}>
@@ -99,7 +67,7 @@ function Weather({setLoggined}) {
                     </Card>
                 </div>
             </div>
-            }
+            {/* } */}
             <br></br>
             <WeatherHistory />
         </div>
